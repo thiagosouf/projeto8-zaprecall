2fc58e3aa79a3b3ffc36d49318cfe8e99a6ac9bd
thiagosouf <thiagosouf@gmail.com>
2022-03-17 17:49:39 -0300

    Passando as perguntas pelo map

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,8 +1,7 @@
 import React from "react"
 
 
-
-export default function Perguntas(props) {
+function Perguntas(props) {
 
     const [pergunta, setPergunta] = React.useState(<Pergunta opcao={props.opcao}/>)
     
@@ -30,5 +29,23 @@ function Card(props){
             <span>{props.card}</span>
             <img  className="setinha" src="./img/setinha.png"></img>
         </div>
+    )
+}
+
+export default function ChamarPerguntas(){
+    const parametros = [
+        {opcao:"Pergunta 1", card:"O que é JSX?"},
+        {opcao:"Pergunta 2", card:"O React é __"},
+        {opcao:"Pergunta 3", card:"Componentes devem iniciar com __"},
+        {opcao:"Pergunta 4", card:"Podemos colocar __ dentro do JSX"},
+        {opcao:"Pergunta 5", card:"O ReactDOM nos ajuda __"},
+        {opcao:"Pergunta 6", card:"Usamos o npm para __"},
+        {opcao:"Pergunta 7", card:"Usamos props para __"},
+        {opcao:"Pergunta 8", card:"Usamos estado (state) para __"}]
+    return(
+        <>
+        {parametros.map(dados =>
+            <Perguntas opcao={dados.opcao} card={dados.card}/>)}
+        </>
     )
 }
--- src/TelaPerguntas.js
+++ src/TelaPerguntas.js
@@ -1,5 +1,6 @@
 
-  import Perguntas from "./Perguntas"
+  import ChamarPerguntas from "./Perguntas"
+import Perguntas from "./Perguntas"
   
   export default function TelaPerguntas() {
     
@@ -11,14 +12,7 @@
                 <span className="tituloPadrao">ZapRecall</span>
             </header>
             <nav>
-                <Perguntas opcao="Pergunta 1" card="O que é JSX?"/>
-                <Perguntas opcao="Pergunta 2" card="O React é __"/>
-                <Perguntas opcao="Pergunta 3" card="Componentes devem iniciar com __"/>
-                <Perguntas opcao="Pergunta 4" card="Podemos colocar __ dentro do JSX"/>
-                <Perguntas opcao="Pergunta 5" card="O ReactDOM nos ajuda __"/>
-                <Perguntas opcao="Pergunta 6" card="Usamos o npm para __"/>
-                <Perguntas opcao="Pergunta 7" card="Usamos props para __"/>
-                <Perguntas opcao="Pergunta 8" card="Usamos estado (state) para __"/>
+                <Perguntas />
             </nav>
             <footer>
                 <span>0/4 CONCLUíDOS</span>
