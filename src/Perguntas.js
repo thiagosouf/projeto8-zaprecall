cdea36c86badef3e1290904b384cbd7175254fcf
thiagosouf <thiagosouf@gmail.com>
2022-03-19 13:37:16 -0300

    embaralhando perguntas

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,4 +1,17 @@
 import React from "react"
+
+let parametros = [
+    {opcao:"Pergunta 1", card:"O que é JSX?", resposta:"Uma extensão de linguagem do JavaScript"},
+    {opcao:"Pergunta 2", card:"O React é __", resposta:"Uma biblioteca JavaScript para construção de interfaces"},
+    {opcao:"Pergunta 3", card:"Componentes devem iniciar com __", resposta:"Letra maiúscula"},
+    {opcao:"Pergunta 4", card:"Podemos colocar __ dentro do JSX", resposta:"Expressões"},
+    {opcao:"Pergunta 5", card:"O ReactDOM nos ajuda __", resposta:"Interagindo com a DOM para colocar componentes React na mesma"},
+    {opcao:"Pergunta 6", card:"Usamos o npm para __", resposta:"Gerenciar os pacotes necessários e suas dependências"},
+    {opcao:"Pergunta 7", card:"Usamos props para __", resposta:"Passar diferentes informações para componentes "},
+    {opcao:"Pergunta 8", card:"Usamos estado (state) para __", resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}]
+
+parametros = parametros.sort( () => Math.random() -0.5 )
+
 function Perguntas(props) {
     
     const [pergunta, setPergunta] = React.useState(<Pergunta  icone={"./img/play.png"} opcao={props.opcao} styleTxt={'none'} styleCor={""}/>)
@@ -54,21 +67,13 @@ function Card(props){
 
 export default function ChamarPerguntas(props){
 
-    const parametros = [
-        {opcao:"Pergunta 1", card:"O que é JSX?", resposta:"Uma extensão de linguagem do JavaScript"},
-        {opcao:"Pergunta 2", card:"O React é __", resposta:"Uma biblioteca JavaScript para construção de interfaces"},
-        {opcao:"Pergunta 3", card:"Componentes devem iniciar com __", resposta:"Letra maiúscula"},
-        {opcao:"Pergunta 4", card:"Podemos colocar __ dentro do JSX", resposta:"Expressões"},
-        {opcao:"Pergunta 5", card:"O ReactDOM nos ajuda __", resposta:"Interagindo com a DOM para colocar componentes React na mesma"},
-        {opcao:"Pergunta 6", card:"Usamos o npm para __", resposta:"Gerenciar os pacotes necessários e suas dependências"},
-        {opcao:"Pergunta 7", card:"Usamos props para __", resposta:"Passar diferentes informações para componentes "},
-        {opcao:"Pergunta 8", card:"Usamos estado (state) para __", resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}]
-
-
+    
     return(
         <>
-        {parametros.map(dados =>
-            <Perguntas contadorSoma={props.contadorSoma} criaIcones={props.criaIcones} opcao={dados.opcao} card={dados.card} resposta={dados.resposta}/>)}
+        {parametros.map((dados,indice) =>
+            <Perguntas contadorSoma={props.contadorSoma} criaIcones={props.criaIcones} opcao={`Pergunta ${indice+1}`} card={dados.card} resposta={dados.resposta}/>)}
+                                                                                       
+        
         </>
     )
 }
